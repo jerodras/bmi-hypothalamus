ae16c3bd52679ee1107d957294b319fcb5581910
Jerod Rasmussen <rasmussj@Jerods-MBP-5.home>
2021-04-29 17:39:51 -0700

    Fancy pliotting

--- MD_BMI_paper_stats_viz.R
+++ MD_BMI_paper_stats_viz.R
@@ -34,10 +34,32 @@ hypoth_data = read.csv('./uci_bmi_hth_covars.csv')
 
 #Continuous BMI, Restricting to BMI threshold (visual inspection from FinnBrain)
 bmi_threshold=36
-bmi_linear_thr_36 <- lm(hth_md ~ ga + sa + sexBinary + bmi + wm_md + qc_mean_fd, data=filter(hypoth_data, bmi < bmi_threshold))
-summary(bmi_linear_thr_36)
-rsq.partial(bmi_linear_thr_36)
-crPlots(bmi_linear_thr_36,hth_md ~ bmi, smooth = FALSE)
+bmi_linear_thr <- lm(scale(hth_md) ~ ga + sa + sexBinary + bmi + wm_md + qc_mean_fd, data=filter(hypoth_data, bmi < bmi_threshold))
+summary(bmi_linear_thr)
+rsq.partial(bmi_linear_thr)
+# #Residual plot, not used because of formatting option
+# crPlots(bmi_linear_thr,hth_md ~ bmi, smooth = FALSE)
+#Create dataset wioth fixed BMI
+hypoth_data_fixed_bmi=hypoth_data
+hypoth_data_fixed_bmi$bmi=mean(hypoth_data$bmi, na.rm=TRUE)
+#Get partial residual
+hypoth_data$model_partial_residual=scale(hypoth_data_fixed_bmi$hth_md)-predict.lm(bmi_linear_thr,hypoth_data_fixed_bmi)
+#Filter using the above threshold
+hypoth_data_thresh=filter(hypoth_data, bmi < bmi_threshold)
+#Plot it, creating x and y just for brevity
+hypoth_data_thresh$x=hypoth_data_thresh$bmi
+hypoth_data_thresh$y=hypoth_data_thresh$model_partial_residual
+
+reg <- lm(y ~ x, hypoth_data_thresh)
+g <- ggplot(hypoth_data_thresh, aes(x = x, y = y))
+g +
+  geom_abline(intercept = coefficients(reg)[1], slope = coefficients(reg)[2], color = "gray", size = 1.5, linetype = "dashed") +
+  ggtitle("BMI vs. Hypothalamus") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_point(color = rgb(0.1875,0.4375,0.7148), alpha = 1, size = 4) +
+  labs(x = "Maternal BMI", y = "Offspring Hypothalamus MD (z-score)") + 
+  theme(panel.background = element_rect(fill = "white", color = "gray", size = 0.5)) +
+  coord_fixed(ratio = 2) 
 
 # #Categorical cut, WHO: healthy vs. overweight
 # hypoth_data$bmi_category <- cut(hypoth_data$bmi, breaks=c(0, 25, 100), labels=c("healthy","overweight"))
